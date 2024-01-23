84138f77e19d8d9bbdfa518f73101ceaf14a18b3
rardooba <rardooba@gmail.com>
2024-01-23 10:32:35 +0100

    🚧 map Study

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // forEach(arr, callback)
-const numbers = [1, 2, 3]
+const numbers = [1, 2, 3, 34, -5, 0]
 
 const map = (arr, callback) => {
 
   let newArr = []
 
   for(let i = 0; i < arr.length; i++) {
-    newArr.push(callback(arr[i]))
+    if(callback(arr[i])) {
+      newArr.push(arr[i])  
+    } 
+
   }
 
   return newArr
 
+
 }
 
 
-console.log(map(numbers, (item) => item * 2))
+console.log(map(numbers, (item) => item > 2))
