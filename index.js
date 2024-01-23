9ae854034d1944fdd12cc2be152312ebbbb7a14b
rardooba <rardooba@gmail.com>
2024-01-23 10:48:20 +0100

    🚧 map Workout

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
 // forEach(arr, callback)
 const numbers = [1, 2, 3, 34, -5, 0]
 
-// const filter = (arr, callback) => {
+const filter = (arr, callback) => {
 
-//   let newArr = []
+  let newArr = []
 
-//   for(let i = 0; i < arr.length; i++) {
-//     if(callback(arr[i])) {
-//       newArr.push(arr[i])  
-//     } 
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      newArr.push(arr[i])  
+    } 
 
-//   }
-
-//   return newArr
-
-
-// }
-
-
-// console.log(filter(numbers, (item) => item > 2))
-
-const users = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Bob" },
-];
-
-const map = (arr, callback) => {
-  const result = [] // newArr
-
-  for (let i = 0; i < arr.length; i++) {
-	const item = callback(arr[i], i, arr) 
-    result.push(item)
   }
 
-  return result
+  return newArr
+
+
 }
 
-console.log(map(numbers, (item) => {
-  return `<li>${item}</li>`
-}))
+
+console.log(filter(numbers, (item) => item % 2 === 0))
