371c1d551fce483f8a39c6f094e8217c4e259095
rardooba <rardooba@gmail.com>
2024-02-08 23:44:12 +0100

    🚧 transform object to array

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// const user = {
-//   pseudo: "Rar",
-//   character: "A.K.I",
-//   mail: "rar@dooba.xyz",
-//   reputation: 23,
-//   isAdmin: false
-// }
+const user = {
+  username: "Rar",
+  age: 23
+}
 
-const array = [2, 34, 2, 123, 75, 8]
-
-const theHigherNumber = array.reduce((acc, cur) => cur + acc, 0)
-
-console.log(theHigherNumber)
+const sayHelloArray = (obj) => Object.values(obj)
+console.log(sayHelloArray(user))
