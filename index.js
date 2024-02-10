4f4c2ea1ceeb5e7e278ab3517fdd60678d38e6e3
rardooba <rardooba@gmail.com>
2024-02-10 23:11:02 +0100

    🚧 case study algo #1

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const user = {
-  username: "Rar",
-  age: 23
+const numbers = [5, 1.3, 23, -0.5, 3.7, 345, 10, -6,7]
+
+const closestToZero = (arr) => {
+
+  let closest = arr[0]
+
+  for (const number of arr) {
+    const absNumber = Math.abs(number)
+    const absClosest = Math.abs(closest)
+    if (absNumber < absClosest) {
+      closest = number
+    } else if (absNumber === absClosest) {
+      closest = number
+    }
+  }
+
+  return closest
 }
 
-const sayHelloArray = (obj) => Object.values(obj)
-console.log(sayHelloArray(user))
+console.log(closestToZero(numbers))
