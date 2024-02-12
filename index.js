44637a3db19329498fcab352aad65a50fe8fb988
rardooba <rardooba@gmail.com>
2024-02-12 19:46:06 +0100

    🚧 remember sort()

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 const numbers = [2.3, -0.34, 0.2, -3, 9, 199, 0.2]
 
-const closestToZero = (numbers) => {
-  let closest = numbers[0]
-  numbers.length === 0 && 0
-
-  for (const number of numbers) {
-    const numberAbs = Math.abs(number)
-    const closestAbs = Math.abs(closest)
-
-    if (numberAbs < closestAbs) {
-      closest = number
-    } else if (numberAbs === closestAbs) {
-      closest = number
-    }
-  }
-
-  return closest
-}
-
-console.log(closestToZero(numbers))
+console.log(numbers.sort((a, b) => a - b))
