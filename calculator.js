2b0196b60ab1853b8cb7784a0995170a7ab1c60f
rardooba <rardooba@gmail.com>
2024-02-20 23:41:18 +0100

    ⚡Maj calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 import { prompt } from "./helper.js";
 
-const LIMIT_NUMBER =  100000000000000
+const LIMIT_NUMBER = 100000000000000;
 
 console.log(`
 ADDITION-MASTER ™️
@@ -10,50 +10,58 @@ Choose an operator:
 2. Soustraction
 3. Multiplication
 4. Division
-`)
+`);
 
-let operator =  Number(await prompt("Enter operator number : "));
 
-while (operator !== 1 && operator !== 2 && operator !== 3 && operator !== 4) {
-  console.error("operator is not a number or choose operator")
-  operator =  Number(await prompt("Enter operator number : "))
+
+const promptOperator = async() => {
+  let operator = Number(await prompt("Enter operator number : "))
+
+  if (operator !== 1 && operator !== 2 && operator !== 3 && operator !== 4) {
+    return promptOperator();
+  }
+
+  return operator
 }
+
+let operator = await promptOperator()
+
+// while (operator !== 1 && operator !== 2 && operator !== 3 && operator !== 4) {
+//   console.error("operator is not a number or choose operator");
+//   operator = Number(await prompt("Enter operator number : "));
+// }
 
 const validateNumber = (number, limitNumber) => {
   if (Number.isNaN(number) || Math.abs(number) > limitNumber) {
-    console.error(`Error : ${number} is not a number or is too big / too small (max: ${limitNumber})`)
-    process.exit(1)
-  }  
+    console.error(
+      `Error : ${number} is not a number or is too big / too small (max: ${limitNumber})`
+    );
+    process.exit(1);
+  }
 
-  return number
-}  
+  return number;
+};
 
 const promptNumber = async (message) => Number(await prompt(`${message} : `));
 
-const firstNumber =  validateNumber(await promptNumber("Enter the first number"), LIMIT_NUMBER);
-const secondNumber =  validateNumber(await promptNumber("Enter the second number"), LIMIT_NUMBER);
+const firstNumber = validateNumber(
+  await promptNumber("Enter the first number"),
+  LIMIT_NUMBER
+);
+const secondNumber = validateNumber(
+  await promptNumber("Enter the second number"),
+  LIMIT_NUMBER
+);
 
-const add = (a, b) => a + b
-const minus = (a, b) => a - b
-const multiply = (a, b) => a * b
-const division = (a, b) => a === 0 || b === 0 ? process.exit(1) : a / b
+const calculateResult = (operator, firstNumber, secondNumber) => {
+  if (operator === 1) console.log(firstNumber + secondNumber);
+  if (operator === 2) console.log(firstNumber - secondNumber);
+  if (operator === 3) console.log(firstNumber * secondNumber);
+  if (operator === 4)
+    console.log(firstNumber === 0 || secondNumber === 0
+      ? process.exit(1)
+      : firstNumber / secondNumber);
+  return;
+};
 
-switch (operator) {
-  case 1:
-    console.log(`the result of addition is : ${add(firstNumber, secondNumber)}`)
-    break;
-  case 2:
-    console.log(`the result of soustration is : ${minus(firstNumber, secondNumber)}`)
-  break
-  case 3:
-  console.log(`the result of multiplication is : ${multiply(firstNumber, secondNumber)}`)
-  break
-  case 4:
-    console.log(`the result of division is : ${division(firstNumber, secondNumber)}`)
-    break
-  default:
-    break;
-}
-
-
-
+calculateResult(operator, firstNumber, secondNumber);
