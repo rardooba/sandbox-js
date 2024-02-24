b8fd9fca6b1279b5af1cb49d670d1d6f3b5d962d
rardooba <rardooba@gmail.com>
2024-02-24 18:43:19 +0100

    🚧 split review

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Week-end en famille ! 
 
-const obj = [
-  {username: "Rar", age: 12},
-  {username: "Dooba", age: 14}
-]
+const str = "Rardooba"
 
-console.log(obj.map(({username, age}) => [username, age]))
+console.log([...str])
