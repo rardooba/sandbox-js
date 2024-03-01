d5ea457819c435c42a0deca678c4d48b9b28a9ce
rardooba <rardooba@gmail.com>
2024-03-01 11:38:46 +0100

    Holiday

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-class Animal {
-  constructor(name, gender) {
-    this.name = name;
-    this.gender = gender;
-  }
-
-  sayHello() {
-    console.log(this.name)
-  }
-}
-
-const cat = new Animal("Rajha", "Cat")
-
-console.log(cat.sayHello())
+// Vacances scolaire kids ! 
