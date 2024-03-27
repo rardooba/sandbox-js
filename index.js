801452549cb0927751726bb2f0241488b4f3e328
rardooba <rardooba@gmail.com>
2024-03-27 17:07:12 +0100

    🚧 Closures

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// I'll be back tomorrow ! 💯
+const PushButton = () => {
+  console.log(`
+  👇
+  🛎️
+  `)
+} 
+const home = () => {
+const tv = "📺" 
+const tvOff = "🚫"
+let isTv = false
+  const telecommad = {
+    on: () => {
+      isTv = true
+      PushButton
+    },
+    off: () => {
+      isTv = false
+      PushButton
+    },
+    getTv: () => {
+      if(isTv) return tv
+      return tvOff
+    }
+  }
+
+  return telecommad
+}
+
+const telecommad = home()
+
+PushButton()
+telecommad.on()
+console.log("Show tv: ", telecommad.getTv())
+PushButton()
+telecommad.off()
+console.log("Show tv: ", telecommad.getTv())
+
+
+
