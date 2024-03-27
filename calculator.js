b1586e1e5dfefee7ffe5ae979cd2fc880bf8f51d
rardooba <rardooba@gmail.com>
2024-03-27 15:44:56 +0100

    ✨MAJ Calculator

--- calculator.js
+++ calculator.js
@@ -1,62 +1,54 @@
 import { prompt } from "./helper.js";
 
-const LIMIT_NUMBER = 100000000000000;
+console.log("ADDITION-MASTER ™️");
 
-console.log(`
-ADDITION-MASTER ™️
-
-Choose an operator:
+console.log(`Choose an operator :
 1. Addition
 2. Soustraction
 3. Multiplication
-4. Division
-`);
+4. Division`);
 
-let operator
+const LIMIT_NUMBER = 100000000000000;
 
-const promptOperator = async(operator) => {
-  operator = Number(await prompt("Enter operator number : "))
-
-  if (operator !== 1 && operator !== 2 && operator !== 3 && operator !== 4) {
-    return promptOperator(operator);
-  }
-
-  return operator
-}
-
-operator = await promptOperator(operator)
-
-const validateNumber = (number, limitNumber) => {
-  if (Number.isNaN(number) || Math.abs(number) > limitNumber) {
+const validateNumber = (number) => {
+  if (Number.isNaN(number) || Math.abs(number) > LIMIT_NUMBER) {
     console.error(
-      `Error : ${number} is not a number or is too big / too small (max: ${limitNumber})`
+      "Error : secondNumber is not a number or is too big / too small (max: 100000000000000)"
     );
     process.exit(1);
   }
-
-  return number;
 };
 
-const promptNumber = async (message) => Number(await prompt(`${message} : `));
-
-const firstNumber = validateNumber(
-  await promptNumber("Enter the first number !"),
-  LIMIT_NUMBER
-);
-const secondNumber = validateNumber(
-  await promptNumber("Enter the second number !"),
-  LIMIT_NUMBER
-);
+const promptNumber = (message) => {
+  const number = Number(prompt(message));
+  validateNumber(number);
+  return number;
+};
 
 const calculateResult = (operator, firstNumber, secondNumber) => {
   if (operator === 1) return firstNumber + secondNumber;
   if (operator === 2) return firstNumber - secondNumber;
   if (operator === 3) return firstNumber * secondNumber;
-  if (operator === 4)
-    return firstNumber === 0 || secondNumber === 0
-      ? process.exit(1)
-      : firstNumber / secondNumber;
-  return 0;
+  if (operator === 4 && secondNumber === 0)
+    throw new Error("Error : division by 0");
+  if (operator === 4) return firstNumber / secondNumber;
 };
+
+const promptOperator = () => {
+  let operator = promptNumber("Enter the operator : ");
+
+  if (operator !== 1 && operator !== 2 && operator !== 3 && operator !== 4) {
+    console.error("Operator must be 1, 2, 3 or 4 !");
+    return promptOperator();
+  }
+
+  return operator;
+};
+
+const operator = promptOperator();
+
+const firstNumber = promptNumber("Enter the first number : ");
+
+const secondNumber = promptNumber("Enter the second number : ");
 
 console.log(calculateResult(operator, firstNumber, secondNumber));
