29763cc39b89c09f9b66f96efe6f5c4fdb414583
rardooba <rardooba@gmail.com>
2024-03-27 18:06:18 +0100

    🚧 Closure printPyramid

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const printPyramid = (height) => {
 
-  for (let i = 0; i < height; i++){
-    let row = " ".repeat(height - i - 1)
-    row += "#".repeat(i * 2 + 1)
+  const addLine = (index) => {
+    let row = " ".repeat(height - index - 1) //4
+    row += "#".repeat(index * 2 + 1)
     console.log(row)
   }
-
+  
+  for (let i = 0; i < height; i++){
+    addLine(i)
+  }
 }
 
-printPyramid(5)
+printPyramid(10)
