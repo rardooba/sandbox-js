1f900f1880a7e618f46a6f7ae5d4b8ef8e87becd
rardooba <rardooba@gmail.com>
2024-03-29 20:04:39 +0100

    🚧 test Closure

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
+const caluculateNumbers = (firstNumber) => function multiply(secondNumber) {
+  return firstNumber * secondNumber
+}
 
-const fn = () => console.log("Hello");
+const result = caluculateNumbers(2)
 
-const rateLimiter = (maxCalls) => {
-
-  let calls = 0
-  const limiter = (callback) => {
-    if (calls >= maxCalls) return
-    callback()
-    calls++
-  }
-
-  return limiter
-};
-
-const max3Calls = rateLimiter(3);
-
-
-max3Calls(fn); // Hello
-max3Calls(fn); // Hello
-max3Calls(fn); // Hello
-max3Calls(fn); // Ne fait rien, car on a atteint la limite d'appels
-
+console.log(result(3))
